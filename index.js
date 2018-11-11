ac6bb6b606e777e179d840232b0d619af6c03b13
nikita <nikitatuhai@gmail.com>
2018-11-11 20:22:35 +0200

    dz

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 "use strict";
 
 let userInput;
-const numbers = [];
+let numbers = [];
 let total = 0;
 
 do {
-  userInput = +prompt("Введите число:");
+  userInput = prompt("Введите число:");
+  const currentInptut = +userInput;
 
-  if (isNaN(userInput)) {
+  if (isNaN(currentInptut) || userInput === "") {
     alert("Было введено не число, попробуйте еще раз");
   } else {
-    numbers.push(userInput);
+    numbers.push(currentInptut);
   }
-} while (userInput !== null && userInput !== 0);
-
-for (let i = 0, max = numbers.length; i < max; i++) {
-  total += numbers[i];
+} while (userInput !== null);
+  
+for(let i of numbers){
+  total += i;
 }
+
 if (total !== 0 && total !== NaN) {
   alert(`Общая сумма чисел равна ${total}`);
 }
